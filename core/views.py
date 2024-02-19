76c4648ebe6fc1e619886bff45d594c62064607c
Sujan <suzan7zoc@gmail.com>
2024-02-19 20:51:35 +0545

    used django form for contact

--- core/views.py
+++ core/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import ContactForm
+from django.contrib import messages
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'core/home.html')
+    date = datetime.now()
+    if request.method=='POST':
+        fm = ContactForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, 'Message sent successfully')
+            
+    else:
+        fm = ContactForm()
+    return render(request, 'core/home.html', {'form':fm, 'date':date})
+
+# def contact_me(request):
+#     if request.method=='POST':
+#         fm = ContactForm(request.POST)
+#         if fm.is_valid():
+#             fm.save()
+#             messages.success(request, 'Message sent successfully')
+#     else:
+#         fm = ContactForm()
+#     return render(request, 'core/contact.html', {'form':fm})
